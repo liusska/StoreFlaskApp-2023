f72294eb0cdc247b78d4331c1124949b391b29b6
Lyudmila Sokolova <lyudmila.sokolovaa@gmail.com>
2023-05-16 15:39:48 +0300

    JWT claims and authorization

--- app.py
+++ app.py
@@ -30,6 +30,13 @@ def create_app(db_url=None):
     app.config["JWT_SECRET_KEY"] = "sometestsecretkey"
     jwt = JWTManager(app)
 
+    @jwt.additional_claims_loader
+    def add_claim_to_jwt(identity):
+        if identity == 1:
+            return {"is_admin": True}
+        return {"is_admin": False}
+
+
     @jwt.expired_token_loader
     def expired_token_callback(jwt_header, jwt_payload):
         return (
