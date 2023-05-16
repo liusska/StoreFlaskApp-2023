d5d6cdac4f4ce049d7ce6d0ca29d679d355d4878
Lyudmila Sokolova <lyudmila.sokolovaa@gmail.com>
2023-05-16 16:30:13 +0300

    Added logout endpoint

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from db import db
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
+from blocklist import BLOCKLIST
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
@@ -29,6 +30,21 @@ def create_app(db_url=None):
 
     app.config["JWT_SECRET_KEY"] = "sometestsecretkey"
     jwt = JWTManager(app)
+
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
+
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {
+                    "description": "The token has been revoked.",
+                    "error": "token_revoked"
+                }
+            )
+        )
 
     @jwt.additional_claims_loader
     def add_claim_to_jwt(identity):
--- resources/user.py
+++ resources/user.py
@@ -1,11 +1,12 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt
 
 from db import db
 from models import UserModel
 from schemas import UserSchema
+from blocklist import BLOCKLIST
 
 
 blp = Blueprint("Users", "users", description="Operations on users")
@@ -39,6 +40,16 @@ class UserLogin(MethodView):
             access_token = create_access_token(identity=user.id)
             return {"access_token": access_token}
         abort(401, message="Invalid credentials.")
+
+
+@blp.route("/logout")
+class UserLogout(MethodView):
+    @jwt_required()
+    def post(self):
+        jti = get_jwt()["jti"]
+        # Good practice is to store it to db
+        BLOCKLIST.add(jti)
+        return {"message": "Successfully logged out."}
 
 
 @blp.route("/user/<int:user_id>")
